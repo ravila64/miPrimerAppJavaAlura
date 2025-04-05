caf0cc89d756b040ddf9f4dccdf0283880db7057
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2025-04-05 16:58:15 -0500

    add Contar.java

---
+++ src/examples/Contar.java
@@ -0,0 +1,11 @@
+package examples;
+
+public class Contar {
+    public static void main(String[] args) {
+        int contar = 1;
+        while (contar <=10 ){
+            System.out.println("Numero "+contar);
+            contar++;
+        }
+    }
+}
